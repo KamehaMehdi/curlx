ecf419285bd6fcd2503548e0672a087cfef21466
KamehaMehdi <dahmammehdi@gmail.com>
2019-07-15 21:05:02 +0200

    Add collection:all run all request in a collection

--- cmds/run.js
+++ cmds/run.js
@@ -18,16 +18,32 @@ module.exports = (args, db) => {
     let collectionRequest = request_id.split(':');
     collectionName = collectionRequest[0];
 
-    if (!db.getCollection(collectionName)) {
-      return outputCollectionNotExists();
-    }
-    let req_id = collectionRequest[1];
-    req = db.getRequestFromCollection(collectionName, req_id);
-  }
+        if (!db.getCollection(collectionName)) {
+            return outputCollectionNotExists();
+        }
+        if(collectionRequest[1] === 'all'){
+          db.getCollections()[collectionName].map(async request => {
+            req = await db.getRequestFromCollection(collectionName, request.id);
+            if (req) {
+              await require('./curlx')(args, req.command, db);
+              await console.log('                                               ')
+              await console.log('===============================================')
+              await console.log('                                               ')
+            } else {
+              await outputRun404(collectionName);
+            }
+          })
 
-  if (req) {
-    require('./curlx')(args, req.command, db);
-  } else {
-    outputRun404(collectionName);
-  }
+        }else{
+            let req_id = collectionRequest[1];
+            console.table({collection: collectionName, Request: req_id })
+            req = db.getRequestFromCollection(collectionName, req_id);
+
+            if (req) {
+              require('./curlx')(args, req.command, db);
+            } else {
+              outputRun404(collectionName);
+            }
+        }
+    }
 }
